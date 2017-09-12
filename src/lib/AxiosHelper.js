764accd976ff183c8d6eef0be12da595c072fb83
Rafael Maciel <faelrrrr@gmail.com>
2017-09-12 11:01:45 -0300

    Modifies the AxiosHelper
    -Adds new methods to contact the api.
    -Modifies header for get, post and patch.

--- src/lib/AxiosHelper.js
+++ src/lib/AxiosHelper.js
@@ -3,8 +3,16 @@ var axios = require('axios');
 axios.defaults.baseURL = 'http://zssn-backend-example.herokuapp.com/api/';
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.patch['Accept'] = 'application/json';
+axios.defaults.headers.patch['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.get['Accept'] = 'application/json';
 
-export function createPerson(personJson) {
+export const createPerson = (queryStr) => axios.post('/people.json', queryStr);
 
-  return axios.post('/people.json', personJson);
-}
+export const getPersonByUUID = uuid => axios.get(`/people/${uuid}.json`);
+
+export const getItems = uuid => axios.get(`/people/${uuid}/properties.json`)
+
+export const saveNewPosition = (uuid, queryStr) => (
+  axios.patch(`/people/${uuid}.json`, queryStr)
+)
