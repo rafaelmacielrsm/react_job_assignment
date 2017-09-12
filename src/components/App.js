92fd5a0fead0c367757d1522d0d18db67e585131
Rafael Maciel <faelrrrr@gmail.com>
2017-09-12 11:00:04 -0300

    Adds a react-router-dom component to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Favicon from 'react-favicon';
 import { Component } from 'react'
-
+import { BrowserRouter, Route, Switch, NavLink, withRouter }
+  from 'react-router-dom'
 import { Login } from './ui/Login'
 import { Dashboard } from './ui/Dashboard'
 
@@ -26,7 +27,10 @@ export class App extends Component {
         <Favicon url="/zombie.png"/>
 
         {user ?
-          <Dashboard uuid={this.state.user}/>
+          <BrowserRouter>
+            <Route path="/" render={ (match) =>
+              <Dashboard uuid={this.state.user} match={match}/> }/>
+          </BrowserRouter>
           : <Login handleUser={this.handleNewUser}/>
         }
 
